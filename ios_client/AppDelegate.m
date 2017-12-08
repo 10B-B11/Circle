564d1cc4083de09354be32b221dc530f9792aa3c
caiyajing9210 <33420849+caiyajing9210@users.noreply.github.com>
2017-12-08 01:41:53 -0500

    Add files via upload

---
+++ ios_client/AppDelegate.m
@@ -0,0 +1,40 @@
+
+
+#import "AppDelegate.h"
+#import "SCTabBarController.h"
+#import "SCLocationManager.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [self updateLocation];
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    SCTabBarController *tabBarVC = [SCTabBarController new];
+    self.window.rootViewController = tabBarVC;
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    [[SCLocationManager sharedManager] stopLoadUserLocation];
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    [self updateLocation];
+}
+
+- (void)updateLocation
+{
+    if ([SCLocationManager isLocationServicesEnabled]) {
+        SCLocationManager *locationManager  = [SCLocationManager sharedManager];
+        [locationManager startLoadUserLocation];
+    }
+}
+
+@end
---
+++ ios_client/source/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Social
+//
+//  Created by Li, Ally on 6/24/17.
+//  Copyright © 2017 ninglinli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
